efaf452ccb67418f051127a2bdb824fe63328016
GitUserRavindu <105047448+GitUserRavindu@users.noreply.github.com>
2024-10-14 20:00:43 +0530

    Add image fetch in instruction page

--- frontend/src/pages/Instruction.jsx
+++ frontend/src/pages/Instruction.jsx
@@ -13,6 +13,7 @@ function Instruction() {
   const [plantData, setPlantData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
+  const [APIImageSrc, setAPIImageSrc] = useState('');
 
   const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
@@ -26,6 +27,7 @@ function Instruction() {
       });
       console.log(response.data);
       setPlantData(response.data);
+      fetchImage(response.data[0].category);
     } catch (err) {
       setError('Failed to fetch plant data.', err);
     }
@@ -48,6 +50,18 @@ function Instruction() {
     localStorage.setItem('plantData', JSON.stringify(plantData));
   };
 
+  const fetchImage = async (category) => {
+    try {
+      const response = await axios.get(`https://pixabay.com/api/?key=46509145-b431f737a8d5f8c98a40dfb87&q=${category}&image_type=photo`);
+      // console.log(response.data.hits[0].largeImageURL);
+      if (response.data.hits[0].largeImageURL) {
+        setAPIImageSrc(response.data.hits[0].largeImageURL);
+      }
+    } catch (error) {
+      console.error('Error fetching image:', error);
+    }
+  }
+
   return (
     <div className="instruction-container">
       <h1 className="instruction-title">Plant Instructions</h1>
@@ -69,12 +83,15 @@ function Instruction() {
 
       {error && <p className="error">{error}</p>}
 
+      {APIImageSrc && <center><img src={APIImageSrc} alt="API Image" className="plant-img"/></center>}
+
       {plantData && (
         <div className="plant-data">
           <PlantList plantData={plantData}/>
           <Link to="/bot" className="explain-btn" onClick={handleExplainByBot}>Explain by Bot</Link>
         </div>
       )}
+      
     </div>
   );
 }
